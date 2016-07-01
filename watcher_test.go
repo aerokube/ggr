0ab42659c4374d33673cbb9502d5fbccc7ff59cc
Alexander Andryashin <aandryashin@gmail.com>
2016-07-01 12:52:16 +0300

    Decreased times

--- watcher_test.go
+++ watcher_test.go
@@ -12,13 +12,13 @@ func TestSingleTimer(t *testing.T) {
 	watcher, _ := fsnotify.NewWatcher()
 	defer watcher.Close()
 	call := false
-	watch(watcher, 150*time.Millisecond, func() {
+	watch(watcher, 15*time.Millisecond, func() {
 		call = true
 	})
 	watcher.Events <- fsnotify.Event{Op: fsnotify.Create}
-	<-time.After(100 * time.Millisecond)
+	<-time.After(10 * time.Millisecond)
 	AssertThat(t, call, Is{false})
-	<-time.After(100 * time.Millisecond)
+	<-time.After(10 * time.Millisecond)
 	AssertThat(t, call, Is{true})
 }
 
@@ -27,18 +27,18 @@ func TestMultipleTimer(t *testing.T) {
 	defer watcher.Close()
 
 	call := false
-	watch(watcher, 150*time.Millisecond, func() {
+	watch(watcher, 15*time.Millisecond, func() {
 		call = true
 	})
 	watcher.Events <- fsnotify.Event{Op: fsnotify.Create}
 
-	<-time.After(100 * time.Millisecond)
+	<-time.After(10 * time.Millisecond)
 	watcher.Events <- fsnotify.Event{Op: fsnotify.Create}
 
-	<-time.After(100 * time.Millisecond)
+	<-time.After(10 * time.Millisecond)
 	AssertThat(t, call, Is{false})
 
-	<-time.After(100 * time.Millisecond)
+	<-time.After(10 * time.Millisecond)
 	AssertThat(t, call, Is{true})
 }
 
@@ -47,23 +47,23 @@ func TestTimerCalledOnce(t *testing.T) {
 	defer watcher.Close()
 
 	call := 0
-	watch(watcher, 150*time.Millisecond, func() {
+	watch(watcher, 20*time.Millisecond, func() {
 		call++
 	})
 	watcher.Events <- fsnotify.Event{Op: fsnotify.Create}
 
-	<-time.After(100 * time.Millisecond)
+	<-time.After(10 * time.Millisecond)
 	watcher.Events <- fsnotify.Event{Op: fsnotify.Create}
 
-	<-time.After(100 * time.Millisecond)
+	<-time.After(10 * time.Millisecond)
 	watcher.Events <- fsnotify.Event{Op: fsnotify.Create}
 
-	<-time.After(100 * time.Millisecond)
+	<-time.After(10 * time.Millisecond)
 	watcher.Events <- fsnotify.Event{Op: fsnotify.Create}
 
-	<-time.After(100 * time.Millisecond)
+	<-time.After(10 * time.Millisecond)
 	AssertThat(t, call, EqualTo{0})
 
-	<-time.After(100 * time.Millisecond)
+	<-time.After(20 * time.Millisecond)
 	AssertThat(t, call, EqualTo{1})
 }
