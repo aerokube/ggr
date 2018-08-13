43a69a339836a8b279c7417f81ac8d7cae311d20
Ivan Krutov <vania-pooh@vania-pooh.com>
2018-08-13 16:19:19 +0300

    Using tag for watch version of Ggr instead of new repo

--- travis/docker-push.sh
+++ travis/docker-push.sh
@@ -8,9 +8,9 @@ docker tag $TRAVIS_REPO_SLUG aandryashin/ggr:$1
 mkdir -p watch
 cp ggr-watch watch/ggr
 cp Dockerfile watch/Dockerfile
-docker build -t aandryashin/ggr-watch:$1 watch
+docker build -t aandryashin/ggr:watch-$1 watch
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push $TRAVIS_REPO_SLUG
 docker push $TRAVIS_REPO_SLUG:$1
 docker push aandryashin/ggr:$1
-docker push aandryashin/ggr-watch:$1
+docker push aandryashin/ggr:watch-$1
